7969844563dfc4a56601e82216dbf4f918fcbb2b
Rebeca Rello <rebeca.rello@gmail.com>
2023-03-15 14:35:45 +0100

    BBDD MySQL Finalizados

--- mySql.js
+++ mySql.js
@@ -53,18 +53,149 @@ connection.connect(function(error){
 
 // });
 
-let sql2 = "INSERT INTO groups (name) VALUES (\"Elefantitos\")";
-connection.query(sql2,function(err,result)
-{
-    if(err){
+// let sql = "INSERT INTO grupos(name) VALUES ('Elefantitos')";
+// connection.query(sql,function(err,result)
+// {
+//     if(err){
 
-    console.log(err);
-    }
-    else{
-        console.log("Dato insertado");
-        console.log(result);
-        console.log(result.insertId);
+//     console.log(err)
+//     }
+//     else{
+//         console.log("Dato insertado");
+//         console.log(result);
+//         console.log(result.insertId);
 
-    }
+//     }
 
-});
+// });
+
+    // let direccion = ("ALTER TABLE direccion ADD piso VARCHAR(200)");
+    // connection.query(direccion,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    // let drop = ("ALTER TABLE direccion DROP piso");
+    // connection.query(drop,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+  
+
+    // let drop = ("DROP TABLE direccion");
+    // connection.query(drop,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    // let marks = ("UPDATE marks SET mark = 0");
+    //  connection.query(marks,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    // let consulta = ("SELECT first_name, last_name FROM students")
+    // connection.query(consulta,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    
+    // let profes = ("SELECT teachers_id, first_name, last_name FROM teachers ");
+    // connection.query(profes,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    //     let borrar = ("DELETE FROM marks WHERE date =<'2013-01-01'");
+    // connection.query(borrar,function(err,result)
+    // {
+    //     if(err){
+
+    //     console.log(err)
+    //     }
+    //     else{
+    //         console.log("Dato insertado");
+    //         console.log(result);
+    //         console.log(result.insertId);
+
+    //     }
+
+    // });
+
+    let aprobado =("UPDATE marks SET mark = 5 WHERE mark <5")
+        connection.query(aprobado,function(err,result)
+        {
+            if(err){
+
+            console.log(err)
+            }
+            else{
+                console.log("Dato insertado");
+                console.log(result);
+                console.log(result.insertId);
+
+            }
+
+        });
